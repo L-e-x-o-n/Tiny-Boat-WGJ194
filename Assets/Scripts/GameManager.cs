2258ba20e5b94cd0e9b28de0293bc2d9ed1fa568
Lexon <44340857+L-e-x-o-n@users.noreply.github.com>
2021-03-28 23:49:08 +0200

    Comment
    Test

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,10 @@ using System;
 
 public class GameManager : Singleton<GameManager>
 {
-    //A list of fish, editable in the inspector
+    //A list of fish types and their prices, editable in the inspector
     public List<Fish> GlobalFish = new List<Fish>();
+
+    //A list of upgrades and their prices, editable in the inspector
     public List<UpgradePrice> GlobalUpgrade = new List<UpgradePrice>();
 }
 
